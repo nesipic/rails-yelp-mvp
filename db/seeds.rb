3451e39a352755ced3934556576b7e99f6ba5ae7
NedijkoSipic <nesipic@hotmail.com>
2020-11-11 15:11:52 +0100

    works perfectly,style need work

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+restaurants = Restaurant.create([{ name: 'Dalmatino', address: 'LandsbergStrasse 122', category: 'french' }, { name: 'Bossandero', address: 'Kupreska 12', category: 'belgian' }, { name: 'NoviSadko', address: 'Partizanska 45', category: 'chinese' }, { name: 'Sarajevo', address: 'Bosanska 122', category: 'italian' }, { name: 'Kabajero', address: 'Barcelona 122', category: 'japanese' }])
